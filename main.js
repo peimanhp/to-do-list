b4b32b7b9a3278dfa6edf3c45ee7ee88935fe7b5
peimanhp <peiman_hp@yahoo.com>
2023-04-20 00:44:20 +0330

    delete task done

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const addTaskBtn = document.querySelector(".add-task-btn");
 const newTaskInfo = document.getElementById("add_new");
-const taskInput = document.getElementById('title');
+const taskInput = document.getElementById("title");
+const dateInput = document.getElementById("date");
+const addTaskBtn = document.querySelector(".add-task-btn");
+const addBtn = document.querySelector(".add");
+const cancelBtn = document.querySelector(".cancel");
+const list = document.querySelector("ul");
 
 addTaskBtn.addEventListener("click", addNewTask);
 function addNewTask() {
@@ -8,14 +12,14 @@ function addNewTask() {
   addTaskBtn.classList.add("hide");
 }
 
-const addBtn = document.querySelector(".add");
 addBtn.addEventListener("click", submitTask);
 function submitTask() {
+  addTaskHandler();
+  if (taskInput.value === "" || dateInput.value === "") return;
   newTaskInfo.classList.remove("show");
   addTaskBtn.classList.remove("hide");
 }
 
-const cancelBtn = document.querySelector(".cancel");
 cancelBtn.addEventListener("click", cancelSubmit);
 function cancelSubmit() {
   newTaskInfo.classList.remove("show");
@@ -39,8 +43,7 @@ function addTask(title, date) {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function renderList() {
-  const list = document.querySelector("ul");
+function renderList() {  
   let tasks = getAllTasks();
   let html = "";
   for (const task of tasks) {
@@ -58,17 +61,41 @@ function renderList() {
   list.innerHTML = html;
 }
 
-taskInput.addEventListener('keyup', (e) => {
-  if (e.key === 'Enter') {
+taskInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
     addTaskHandler();
   }
-})
+});
 
 function addTaskHandler() {
-  if (taskInput == '') return;
-  addTask(taskInput.value, date);
-  taskInput.value = '';
+  if (taskInput == "") return;
+  if (dateInput.value === '') {
+    alert('You must select a date!');
+    return;
+  }
+  addTask(taskInput.value, dateInput.value);
+  taskInput.value = "";
+  dateInput.value = "";
   renderList();
 }
+
+function deleteTask(id) {
+  let tasks = getAllTasks();
+  let length = tasks.length;
+  tasks = tasks.filter(function (task) {
+    return task.id != id;
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  return tasks.length != length;
+}
+
+list.addEventListener('click', e => {
+  let target = e.target;
+  if (target.classList.contains('remove-task-btn')) {
+    let id = target.id.substr(11);
+    deleteTask(id);
+    renderList();
+  }
+})
 
 renderList();
